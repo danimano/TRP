46c2e6c56436caa1d7c9c8272f7466ae7bf3b40f
xArchange <candice.bentejac@gmail.com>
2018-01-05 18:31:48 +0100

    Added error handling in the reader.

--- pearlib/pearlib/reader.py
+++ pearlib/pearlib/reader.py
@@ -1,42 +1,58 @@
 import numpy as np
 import tensorflow as tf
 
-
 def read_tensorflow_file(fname):
     new_graph = tf.Graph()
 
     with tf.Session(graph=new_graph) as sess:
-        saver = tf.train.import_meta_graph(fname + '.meta')
+        # Handles case where there is no .meta file
+        try:
+            saver = tf.train.import_meta_graph(fname + '.meta')
+        except OSError:
+            raise OSError("The .META file could not be found!")
+            
         graph = tf.get_default_graph()
 
-        tf.train.Saver().restore(sess, fname)
-        theta = []  # [[0,0]]*int(graph.get_collection_ref('trainable_variables').__len__()/2)
+        # Handles the case where data is corrupted or missing
+        try:
+            tf.train.Saver().restore(sess, fname)
+        except tf.errors.DataLossError:
+            raise tf.errors.DataLossError(None, None, "A file is missing (.INDEX or .DATA), and the network could not be reconstructed, or the data is corrupted!")
+        
+        theta = []
         tmp = []
         tmpW = [0] * int(graph.get_collection_ref('trainable_variables').__len__() / 2)
         tmpb = [0] * int(graph.get_collection_ref('trainable_variables').__len__() / 2)
 
-        # print(theta)
         file_writer = tf.summary.FileWriter(fname, sess.graph)
 
         for x in graph.get_collection_ref('trainable_variables'):
-            # "Legacy code" Our measurements don't have the new naming convention, but I want to upload a version, which works for them.
-            if x.name[0:8] == 'Variable_':
-                if not tmp:
-                    tmp += [x]
-                    continue
-                if tmp[0].shape[1] == x.shape[0]:
-                    theta += [(np.array(tmp[0].eval(sess)), np.array(x.eval(sess)))]
-                    tmp = []
-            # End of legacy
             if x.name[0] == 'W' or x.name[0] == 'b':
                 if x.name[0] == 'W':
                     tmpW[int(x.name[1:-2]) - 1] = x.eval(sess)
                 else:
                     tmpb[int(x.name[1:-2]) - 1] = x.eval(sess)
+                    
     if tmpW.__len__() != tmpb.__len__():
         print("Missing data!")
+        
     for x in range(tmpW.__len__()):
         theta += [(tmpW[x], tmpb[x])]
-    return theta
+
+    # Checking that an actual network was loaded in, and not an empty one
+    empty = True
+    error = []
+    for i in range(0, len(theta)):
+        error.append((0, 0))
+    try:
+        error == theta
+    except Exception:
+        empty = False
+
+    if empty == True:
+        raise ValueError("The network that was loaded in is empty! The input network you used probably does not follow our guidelines!")
+    else:
+        return theta
+        
 
 
